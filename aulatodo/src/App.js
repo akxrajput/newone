d097d39db88ef1c128d2e1cb480edc0a1ad2d018
akshay girase <akshaygirase122003@gmail.com>
2023-11-29 16:45:50 +0530

    commit

---
+++ aulatodo/src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import './App.css';
+// import Tabs from "./component/TabMenu/Tabs";
+
+import Todo from "./Components/todoProject/todo";
+
+
+const App = () => {
+  return (
+    <>
+     <Todo/>
+    
+    </>
+  )
+}
+
+export default App
+
---
+++ aulatodo/src/Components/todoProject/Lastbtn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Lastbtn = () => {
+  return (
+    <>
+     <h1>hello aulacube recruiters , as my endsem exam is going i could'nt much better than this</h1>
+    </>
+  )
+}
+
+export default Lastbtn
+
